84d68e0726928f82ceb81cd76278692bd7f3dd29
william <genwillen@gmail.com>
2025-12-15 12:47:11 +0800

    feat: open broswer & sleep 6s

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+from time import sleep
+
 from playwright.sync_api import sync_playwright
 from utils.config_loader import get_url
 from core.runner import auto_run
@@ -7,11 +10,14 @@ def main():
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
+        # browser = p.webkit.launch(headless=False)
         context = browser.new_context(storage_state="config/login_state.json")
         page = context.new_page()
         page.goto(url)
 
         print("已自动登录，开始自动刷题！")
+        # sleep 6s
+        time.sleep(6)
         auto_run(page)
 
 if __name__ == "__main__":
